30013254d9677ba437db84c0f4e9aba28adec98f
GitHub Action <action@github.com>
2020-09-30 09:17:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/roukanken42/intellijorikaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/roukanken42/intellijorikaplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.roukanken42.intellijorikaplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.roukanken42.intellijorikaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/roukanken42/intellijorikaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.roukanken42.intellijorikaplugin.services
+
+import com.github.roukanken42.intellijorikaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
